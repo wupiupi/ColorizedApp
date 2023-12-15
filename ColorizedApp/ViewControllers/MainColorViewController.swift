c08fe656ebb16c7292372b2a81ea2881243303df
Paul <pasha.makey@gmail.com>
2023-12-15 11:56:36 +0300

    color transfer

--- ColorizedApp/ViewControllers/MainColorViewController.swift
+++ ColorizedApp/ViewControllers/MainColorViewController.swift
@@ -7,9 +7,27 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate: AnyObject {
+    func setViewColor(with color: UIColor)
+}
+
 final class MainColorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let navVC = segue.destination as? UINavigationController else { return }
+        guard let settingsVC = navVC.topViewController as? SettingsViewController else { return }
+        settingsVC.delegate = self
+        settingsVC.color = view.backgroundColor
+    }
+}
+
+// MARK: - SettingsViewControllerDelegate
+extension MainColorViewController: SettingsViewControllerDelegate {
+    func setViewColor(with color: UIColor) {
+        view.backgroundColor = color
     }
 }
--- ColorizedApp/ViewControllers/SettingsViewController.swift
+++ ColorizedApp/ViewControllers/SettingsViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 final class SettingsViewController: UIViewController {
     
+    // MARK: - IB Outlets
     @IBOutlet var colorView: UIView!
     
     @IBOutlet var redValueLabel: UILabel!
@@ -19,17 +20,37 @@ final class SettingsViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
+    weak var delegate: SettingsViewControllerDelegate?
+    var color: UIColor!
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         colorView.layer.cornerRadius = 15
+        colorView.backgroundColor = color
         
         redSlider.tintColor = .red
         greenSlider.tintColor = .green
+        
+        setColors()
     }
     
+    // MARK: - IB Action
     @IBAction func sliderValueDidChanged(_ sender: UISlider) {
+        switch sender {
+            case redSlider: redValueLabel.text = string(from: sender.value)
+            case greenSlider: greenValueLabel.text = string(from: sender.value)
+            default: blueValueLabel.text = string(from: sender.value)
+        }
         updateUI()
     }
+    
+    @IBAction private func saveButtonDidTapped(_ sender: UIBarButtonItem) {
+        delegate?.setViewColor(with: colorView.backgroundColor ?? .black)
+        dismiss(animated: true)
+    }
+    
 }
 
 // MARK: - Private Methods
@@ -38,9 +59,32 @@ private extension SettingsViewController {
         redValueLabel.text = string(from: redSlider.value)
         greenValueLabel.text = string(from: greenSlider.value)
         blueValueLabel.text = string(from: blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
     }
     
     func string(from float: Float) -> String {
         String(format: "%.2f", float)
+    }
+    
+    func string(from cgFloat: CGFloat) -> String {
+        String(format: "%.2f", cgFloat)
+    }
+    
+    func setColors() {
+        let ciColor = CIColor(color: color)
+        
+        redSlider.value = Float(ciColor.red)
+        greenSlider.value = Float(ciColor.green)
+        blueSlider.value = Float(ciColor.blue)
+        
+        redValueLabel.text = string(from: ciColor.red)
+        greenValueLabel.text = string(from: ciColor.green)
+        blueValueLabel.text = string(from: ciColor.blue)
     }
 }
